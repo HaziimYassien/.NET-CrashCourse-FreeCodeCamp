496ef8cad946814cbf76a6a16d969c9b70ee234a
HaziimYassien <haziim.yassien@gmail.com>
2023-09-17 22:17:03 +0300

    10 minutes left

--- JokesWebApp/JokesWebApp/Controllers/JokesController.cs
+++ JokesWebApp/JokesWebApp/Controllers/JokesController.cs
@@ -27,6 +27,21 @@ namespace JokesWebApp.Controllers
                           Problem("Entity set 'ApplicationDbContext.Joke'  is null.");
         }
 
+
+        // GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();   
+        }
+
+        // POST: Jokes/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View();
+        }
+
+
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
